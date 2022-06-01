bc258da59fa3808e944dde2bf58c6b5c8c1de480
BezaB10 <beza_b@outlook.comq qgit config--qgit config -- qgit config --global user.email beza_b@outlook.com>
2022-06-01 03:13:55 -0700

    lastDigit

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
-def print_last_digit(n):
-    tmp = n
+def print_last_digit(number):
+    tmp = number
     if tmp < 0:
-        tmp = (-1) * n
+        tmp = (-1) * number
     last_digit = tmp % 10
     print("{}".format(last_digit), end="")
     return last_digit
