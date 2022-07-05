a53e35b6a62799696f6ba3a533aafd2d763bb74c
BezaB10 <beza_b@outlook.comq qgit config--qgit config -- qgit config --global user.email beza_b@outlook.com>
2022-07-05 10:45:48 -0700

    edited

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -8,7 +8,6 @@ status_list = [200, 301, 400, 401, 403, 404, 405, 500]
 try:
     total_size = 0
     final_list = []
-    
     for index, line in enumerate(sys.stdin, 1):
         if line:
             line_split = line.split()
