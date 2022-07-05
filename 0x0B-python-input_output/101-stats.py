0d107f03492f59871b69b7a9e054e15d1c9a37ee
BezaB10 <beza_b@outlook.comq qgit config--qgit config -- qgit config --global user.email beza_b@outlook.com>
2022-07-05 11:16:49 -0700

    edited

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -36,7 +36,8 @@ try:
 
             if str(status) in status_codes.keys():
                 status_codes[str(status)] += 1
-
+        except:
+            pass
         try:
             file_size += int(pieces[-1])
         except:
