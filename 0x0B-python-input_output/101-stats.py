c652db00e82710929ace10ca753b96d493a8a7ac
BezaB10 <beza_b@outlook.comq qgit config--qgit config -- qgit config --global user.email beza_b@outlook.com>
2022-07-05 10:42:54 -0700

    edited

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 'Log Parsing'
+
 import sys
 
 status_list = [200, 301, 400, 401, 403, 404, 405, 500]
+
 try:
     total_size = 0
     final_list = []
+    
     for index, line in enumerate(sys.stdin, 1):
         if line:
             line_split = line.split()
@@ -26,8 +29,8 @@ except Exception:
     pass
 
 finally:
-    print('File size: {}'.format(total_size))
+    print('File size: {:d}'.format(total_size))
     for i in status_list:
         if i in final_list:
             status_count = final_list.count(i)
-            print("{}: {}".format(i, status_count))
+            print("{}: {:d}".format(i, status_count))
