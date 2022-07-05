33eab5ae9f2d39d0d71cd82c4a22bbcf6c4c3eaf
BezaB10 <beza_b@outlook.comq qgit config--qgit config -- qgit config --global user.email beza_b@outlook.com>
2022-07-05 10:24:09 -0700

    edited

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-"""
-    This is a python script that adds all arguments to a Python List.
-    List is then saved to a file.
-"""
+"""add item"""
 
+
+import json
 import sys
+import os.path
+
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-
 
 filename = "add_item.json"
 if os.path.isfile(filename):
